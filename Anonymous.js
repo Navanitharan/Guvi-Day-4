f91f06834caf2fb8a040b316562134f0201200da
NAVANITHARAN <navanitharan505@gmail.com>
2023-11-25 07:00:40 +0530

    file updated

--- Anonymous.js
+++ Anonymous.js
@@ -41,22 +41,61 @@
 // prime([1,2,3,4,5,6,7,8,9,10,11]);
 
 //5.PALINDROME
-var prime = function(arr) {
-    for(var i=0; i<arr.length;i++){
-        var pal=true;
-        var f=0;
-        var l=arr[i].length-1
-        while(f<l){
-            if(arr[i].charAt(f)!=arr[i].charAt(l) ){
-                pal=false;
-                break;
+// var prime = function(arr) {
+//     for(var i=0; i<arr.length;i++){
+//         var pal=true;
+//         var f=0;
+//         var l=arr[i].length-1
+//         while(f<l){
+//             if(arr[i].charAt(f)!=arr[i].charAt(l) ){
+//                 pal=false;
+//                 break;
+//             }
+//             l--;
+//             f++;
+//         }
+//         if(pal==true){
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+// prime(["madam","lool","car"])
+
+//6.MEDIAN OF SORTED ARRAY
+// var arr1=[1,2,3,4,5];
+// var arr2=[6,7,8,9,10];
+
+// var merge=function(arr1,arr2){
+// var merge=[...arr1,...arr2];
+//    return merge;
+// }
+
+// var marr=merge(arr1,arr2);
+
+// var len=marr.length/2;
+// var median;
+
+// if(marr.length%2==0){
+//     median=(marr[len-1]+marr[len])/2;
+// }else{
+//     median=marr[len-1]/2;
+// }
+// console.log("The Median is: "+Math.round(median))
+
+//7.REMOVE DUPLICATE
+var isdup = function (arr){
+    var nodup=[];
+    for(var i=0; i<arr.length; i++){
+        var count=0;
+        for(var j=i+1;j<arr.length;j++){
+            if(arr[j]==arr[i]){
+                count++;
             }
-            l--;
-            f++;
         }
-        if(pal==true){
-            console.log(arr[i]);
+        if(count==0){
+            nodup.push(arr[i]);
         }
     }
+    console.log(nodup)
 }
-prime(["madam","lool","car"])
+isdup([1,2,3,4,2,6,3])
--- IIFE.js
+++ IIFE.js
@@ -1,4 +1,4 @@
-// //1.odd
+//1.odd
 // (function(arr){
 //     for(let i=0;i<arr.length;i++){
 //         if(arr[i]%2!=0){
@@ -36,21 +36,57 @@
 // }([1,2,3,4,5,6,7,8,9,10,11]))
 
 //5.PALINDROME 
-(function(arr) {
-    for(var i=0; i<arr.length;i++){
-        var pal=true;
-        var f=0;
-        var l=arr[i].length-1
-        while(f<l){
-            if(arr[i].charAt(f)!=arr[i].charAt(l) ){
-                pal=false;
-                break;
+// (function(arr) {
+//     for(var i=0; i<arr.length;i++){
+//         var pal=true;
+//         var f=0;
+//         var l=arr[i].length-1
+//         while(f<l){
+//             if(arr[i].charAt(f)!=arr[i].charAt(l) ){
+//                 pal=false;
+//                 break;
+//             }
+//             l--;
+//             f++;
+//         }
+//         if(pal==true){
+//             console.log(arr[i]);
+//         }
+//     }
+// }(["madam","lool","car"]))
+
+//6.MEDIAN OF SORTED ARRAY
+// var arr1=[1,2,3,4,5];
+// var arr2=[6,7,8,9,10];
+
+// (function(arr1,arr2){
+// var marr=[...arr1,...arr2];
+// var len=marr.length/2;
+// var median;
+
+// if(marr.length%2==0){
+//     median=(marr[len-1]+marr[len])/2;
+// }else{
+//     median=marr[len-1]/2;
+// }
+// console.log("The Median is: "+Math.round(median))
+// })(arr1,arr2);
+
+
+//7.REMOVE DUPLICATE
+(function (arr){
+    var nodup=[];
+    for(var i=0; i<arr.length; i++){
+        var count=0;
+        for(var j=i+1;j<arr.length;j++){
+            if(arr[j]==arr[i]){
+                count++;
             }
-            l--;
-            f++;
         }
-        if(pal==true){
-            console.log(arr[i]);
+        if(count==0){
+            nodup.push(arr[i]);
         }
     }
-}(["madam","lool","car"]))
+    console.log(nodup)
+})([1,2,3,4,2,6,3])
+
